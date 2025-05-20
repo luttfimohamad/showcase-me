eda715c691226aa0de39b6441399c10c04c1ac2a
luttfimohamad <luttfimohamad@gmail.com>
2025-05-20 23:50:14 +0700

    Refactor POST function in route.js to utilize dynamic email data and improve message structure

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -2,16 +2,25 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
+export async function POST(req) {
+  const { email, subject, message } = await req.json();
   try {
     const data = await resend.emails.send({
-      from: '',
-      to: [''],
-      subject: 'Hello world',
+      from: fromEmail,
+      to: 'luttfimohamad@gmail.com',
+      subject: subject,
       react: (
         <>
-          <p>Email Body</p>
+          <h1>{subject}</h1>
+          <p>
+            <strong>From:</strong> {email}
+          </p>
+          <p>
+            <strong>Message:</strong>
+          </p>
+          <p>{message}</p>
         </>
       ),
     });
