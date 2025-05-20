0543ee3841ae12c116b2af5362fdab955b9458b3
luttfimohamad <luttfimohamad@gmail.com>
2025-05-20 12:07:12 +0700

    Add Navbar and NavLink components to enhance site navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/components/hero-section';
+import Navbar from '@/components/Header/nav-bar';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className="container mx-auto px-12 py-4">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
       </div>
     </main>
