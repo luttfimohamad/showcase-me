dc9663b166b0e3c320416c64e5b07b8be840d0ea
luttfimohamad <luttfimohamad@gmail.com>
2025-05-20 15:56:40 +0700

    Add z-index to Footer component for improved layering

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+    <footer className="footer border border-t-[#33353F] z-10 border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span className="bg-gradient-to-r from-rose-500 to-rose-400 bg-clip-text text-transparent">
           Showcase
