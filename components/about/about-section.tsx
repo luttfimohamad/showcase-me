ab2af194b18296737979e190e822bf4bad8995d5
luttfimohamad <luttfimohamad@gmail.com>
2025-05-22 16:54:58 +0700

    Add id attribute to AboutSection for improved accessibility

--- components/about/about-section.tsx
+++ components/about/about-section.tsx
@@ -53,7 +53,7 @@ export default function AboutSection() {
   };
 
   return (
-    <section className="text-white">
+    <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
           src="/images/about-me.png"
