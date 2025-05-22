64c1bc19cbe5b9b7da00bafdbb2ad79e638776b8
luttfimohamad <luttfimohamad@gmail.com>
2025-05-22 16:16:16 +0700

    Refactor AchievementsSection layout for improved responsiveness and styling

--- components/achievements/achievements-section.tsx
+++ components/achievements/achievements-section.tsx
@@ -22,18 +22,20 @@ const achievementsList = [
 export default function AchievementsSection() {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
-        {achievementsList.map((achievement, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center justify-center mx-4"
-          >
-            <h1 className="text-white text-4xl font-bold">
-              {achievement.value}
-            </h1>
-            <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
-          </div>
-        ))}
+      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-17 flex flex-col sm:flex-row items-center justify-between">
+        {achievementsList.map((achievement, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+            >
+              <h2 className="text-white text-4xl font-bold">
+                {achievement.value}
+              </h2>
+              <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
