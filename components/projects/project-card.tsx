f39dd1f71da355711b5428c5b4fa9c32c79a6d8d
luttfimohamad <luttfimohamad@gmail.com>
2025-05-31 15:52:35 +0700

    Update project details and descriptions in ProjectSection component

--- components/projects/project-card.tsx
+++ components/projects/project-card.tsx
@@ -47,7 +47,7 @@ export default function ProjectCard({
 
       <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
-        <p className="text-[#ADB7BE]">{description}</p>
+        <p className="text-[#ADB7BE] text-justify">{description}</p>
       </div>
     </div>
   );
